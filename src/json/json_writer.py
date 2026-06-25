f0d0da4d2192e1fd4ed836df7573663e09ec6383
Matheus-Sounier <matheus.sounier2024@gmail.com>
2026-06-25 15:55:17 -0400

    chore: creates and writes the data, collected while detecting the plates, to output and plate_data folders

--- src/json/json_writer.py
+++ src/json/json_writer.py
@@ -12,3 +12,18 @@ def save_json(license_plates, startTime, endTime):
             for plate, is_valid, image in license_plates
         ]
     }
+
+    interval_file_path = "./src/json/output/output_" + datetime.now().strftime("%Y%m%d%H%M%S") + ".json"
+    with open(interval_file_path, 'w') as f:
+        json.dump(interval_data, f, indent=2)
+
+    cummulative_file_path = "./src/json/plate_data/LicensePlateData.json"
+    if os.path.exists(cummulative_file_path):
+        with open(cummulative_file_path, 'r') as f:
+            existing_data = json.load(f)
+    else:
+        existing_data = []
+
+    existing_data.append(interval_data)
+    with open(cummulative_file_path, 'w') as f:
+        json.dump(existing_data, f, indent=2)
