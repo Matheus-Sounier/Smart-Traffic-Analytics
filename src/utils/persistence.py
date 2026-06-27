f5c4fe454f260f8f257392774842f77987d5c3bf
Matheus-Sounier <matheus.sounier2024@gmail.com>
2026-06-26 23:36:01 -0400

    chore: adds try exception for items extracted from license plate and saved_plates - Adds saved plate, state, image and score of the new plates variable to json file - Adds saved items of new plates to be storege datanbase

--- src/utils/persistence.py
+++ src/utils/persistence.py
@@ -8,18 +8,27 @@ def persist_interval(license_plates, saved_plates, startTime, currentTime):
         return startTime
 
     endTime = currentTime
-    new_plates = {
-        track_id: (plate, is_valid, image, score, confirm_count)
-        for track_id, (plate, is_valid, image, score, confirm_count) in license_plates.items()
-        if plate not in saved_plates and confirm_count >= 2
-    }
-    if new_plates:
-        saved_plates.update(plate for plate, *_ in new_plates.values())
-        save_json(
-            [(plate, is_valid, image, score) for plate, is_valid, image, score, count in new_plates.values()],
-            startTime, endTime
-        )
-        save_to_database(
-            [(plate, is_valid, image) for plate, is_valid, image, score, count in new_plates.values()],
-            startTime, endTime
-        )
+    try:
+        new_plates = {
+            track_id: (plate, is_valid, image, score, confirm_count)
+            for track_id, (plate, is_valid, image, score, confirm_count) in license_plates.items()
+            if plate not in saved_plates and confirm_count >= 2
+        }
+        if new_plates:
+            saved_plates.update(plate for plate, *_ in new_plates.values())
+            save_json(
+              [(plate, is_valid, image, score) for plate, is_valid, image, score, count in new_plates.values()],
+              startTime, endTime
+            )
+            save_to_database(
+              [(plate, is_valid, image) for plate, is_valid, image, score, count in new_plates.values()],
+              startTime, endTime
+          )
+
+    except Exception as e:
+        print(f"Error in persisting data: {e}")
+    finally:
+        for track_id in list(new_plates.keys()):
+            license_plates.pop(track_id, None)
+
+    return endTime
